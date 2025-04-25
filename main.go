82f830bac0192b3a9f389a9e92c7585de2da949f
RbKomar <robkomar98@gmail.com>
2025-04-25 19:36:41 +0200

    upgrade to fileserver serving index.html

--- main.go
+++ main.go
@@ -7,10 +7,11 @@ import (
 	)
 
 func main() {
+	const filePathRoot = ".";
 	const port = "8080";
 
 	mux := http.NewServeMux()
-	
+	mux.Handle("/", http.FileServer(http.Dir(filePathRoot)))
 	server := &http.Server{
 		Addr:":" + port, 
 		Handler:mux,
