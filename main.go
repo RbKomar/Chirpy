17dc7a6437e0be6bf2f5e8271b798c45ffdd5380
RbKomar <robkomar98@gmail.com>
2025-07-17 19:13:04 +0200

    db management with goose and sqlc

--- main.go
+++ main.go
@@ -1,22 +1,37 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/RbKomar/Chirpy/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	dbQueries      *database.Queries
 }
 
 func main() {
+	godotenv.Load()
 	const filePathRoot = "."
 	const port = "8080"
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalln("Connection to db doesn't work.")
+	}
+	dbQueries := database.New(db)
+
 	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
+		dbQueries:      dbQueries,
 	}
-
 	mux := http.NewServeMux()
 	handler := apiCfg.middlewareMetricInc(http.FileServer(http.Dir(filePathRoot)))
 	mux.Handle("/app/", http.StripPrefix("/app/", handler))
